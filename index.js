301ab96dd846507bebb30695debf7e0f5f463c18
mdnumanahmed <mnumansyl@gmail.com>
2023-05-12 10:59:26 +0600

    Added get api for all services

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(cors())
 app.use(express.json())
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@carcluster.bt452kw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,14 +26,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carCareCenter').collection('services')
+
+
+
+    app.get('/services', async(req, res)=>{
+        const cursor = serviceCollection.find()
+        const services = await cursor.toArray()
+        res.send(services)
+    })
+
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
